8287ca6117dc6195d47723ef5f8d36017f541573
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-14 11:45:16 +0700

    fix

--- ctpn/demo.py
+++ ctpn/demo.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
         ckpt = tf.train.get_checkpoint_state(cfg.TEST.checkpoints_path)
         print(ckpt)
         print('Restoring from {}...'.format(ckpt.model_checkpoint_path), end=' ')
-        saver.restore(sess, ckpt.model_checkpoint_path)
+        saver.restore(sess,"/content/text-detection-ctpn/checkpoints/VGGnet_fast_rcnn_iter_50000.ckpt" )
         print('done')
     except:
         raise 'Check your pretrained {:s}'.format(ckpt.model_checkpoint_path)
