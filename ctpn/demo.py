aee2778f2ba37e03422ece5fedee64cad08d19b0
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-14 11:35:08 +0700

    fix

--- ctpn/demo.py
+++ ctpn/demo.py
@@ -64,11 +64,11 @@ def ctpn(sess, net, image_name):
 
 
 if __name__ == '__main__':
-    if os.path.exists("data/results/"):
-        shutil.rmtree("data/results/")
-    os.makedirs("data/results/")
+    if os.path.exists("/content/text-detection-ctpn/data/results/"):
+        shutil.rmtree("/content/text-detection-ctpn/data/results/")
+    os.makedirs("/content/text-detection-ctpn/data/results/")
 
-    cfg_from_file('ctpn/text.yml')
+    cfg_from_file('/content/text-detection-ctpn/ctpn/text.yml')
 
     # init session
     config = tf.ConfigProto(allow_soft_placement=True)
