ac3baf0893bdd1209e1d50083e4c7f2d5ca0b826
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-17 21:07:10 +0700

    fix

--- lib/roi_data_layer/minibatch.py
+++ lib/roi_data_layer/minibatch.py
@@ -65,9 +65,9 @@ def get_minibatch(roidb, num_classes):
             raw_boxs=[[int(number) for number in line[:-1].split(',')[:8]] for line in f.readlines()]
         
         for box in raw_boxs:
-            print((int(box[0]/scale_x), int(box[1]/scale_y)), (int(box[0]/scale_x)+10, int(box[1/scale_y])+10))
+            print((int(box[0]/scale_x), int(box[1]/scale_y)), (int(box[0]/scale_x)+10, int(box[1]/scale_y)+10))
             print(debug_img.shape)
-            cv2.line(debug_img, (int(box[0]/scale_x), int(box[1]/scale_y)), (int(box[0]/scale_x)+10, int(box[1/scale_y])+10), color, 10)
+            cv2.line(debug_img, (int(box[0]/scale_x), int(box[1]/scale_y)), (int(box[0]/scale_x)+10, int(box[1]/scale_y)+10), color, 10)
             # cv2.line(debug_img, (int(box[2]), int(box[3])), (int(box[4]), int(box[5])), color, 10)
 
             # cv2.line(mask, (int(box[2]), int(box[3])), (int(box[2]), int(box[1])), color, 20)
