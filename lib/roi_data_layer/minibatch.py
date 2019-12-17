1ef9b50877f43084d262d25c7e80622db6e101cd
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-17 21:45:52 +0700

    fix

--- lib/roi_data_layer/minibatch.py
+++ lib/roi_data_layer/minibatch.py
@@ -60,7 +60,7 @@ def get_minibatch(roidb, num_classes):
 
         scale_x=raw_image.shape[0]/debug_img.shape[0]
         scale_y=raw_image.shape[1]/debug_img.shape[1]
-        mask=np.zeros(im_blob.shape[1:])
+        mask=(np.zeros(im_blob.shape[1:])+255).astype(np.uint8)
         
         color = (254, 254, 0)
         with open(raw_gt_file,"r") as f:
@@ -74,8 +74,8 @@ def get_minibatch(roidb, num_classes):
             # cv2.line(debug_img,(0,0),(20,20),(0,255,255),20)
             # cv2.line(debug_img, (int(box[1]/scale_y),int(box[0]/scale_x)),
             #  ( int(box[1]/scale_y)+50,int(box[0]/scale_x)+50), color, 100)
-            cv2.line(mask, (int(box[0]/scale_x),int(box[1])),
-             ( int(box[0]/scale_x)+5,int(box[1])+10), color, 5)
+            cv2.line(mask, (int(box[0]/scale_x),int(box[1]/scale_y)),
+             ( int(box[0]/scale_x)+5,int(box[1]/scale_y)+10), color, 5)
             
             # cv2.line(debug_img, (int(box[2]), int(box[3])), (int(box[4]), int(box[5])), color, 10)
 
