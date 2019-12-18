96e197496cef350b6963476d7369d991d6eb4fd1
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-18 17:23:56 +0700

    fix

--- lib/networks/network.py
+++ lib/networks/network.py
@@ -209,6 +209,7 @@ class Network(object):
 
             init_weights = tf.truncated_normal_initializer(0.0, stddev=0.01)
             init_biases = tf.constant_initializer(0.0)
+            print([k_h, k_w, c_i, c_o])
             kernel = self.make_var('weights', [k_h, k_w, c_i, c_o], init_weights, trainable, \
                                    regularizer=self.l2_regularizer(cfg.TRAIN.WEIGHT_DECAY))
             if biased:
